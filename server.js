159a0657b6fc1f807dd33fe066e51667defc4557
Alok-Kumar-1 <alokkumar68338@gmail.com>
2026-03-21 21:56:54 +0530

    Initial Node.js app setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("DevOps CI/CD Project Running 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
