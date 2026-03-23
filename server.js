1f914aee99fb44856df7b9e3fe204e9fcf79ce41
Alok-Kumar-1 <alokkumar68338@gmail.com>
2026-03-23 23:35:35 +0530

    test auto deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("DevOps CI/CD Project Running 🚀");
+  res.send("DevOps CI/CD Project Running 🚀 <br> Hello from CI/CD 🚀");
 });
 
 app.get("/health", (req, res) => {
